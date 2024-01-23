1db872d2582b8b5e864eb1e36c32d7111b8ff086
Pranav <pranavlakkadi@gmail.com>
2024-01-23 22:17:48 +0530

    updates

---
+++ deploy/05-deploy-EncryptedPairFHE.js
@@ -0,0 +1,22 @@
+const { network , ethers } = require("hardhat");
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deployer} = await getNamedAccounts();
+    const {deploy,log} = deployments;
+    const chainid = network.config.chainId;
+    log("deploy to the chainId " + chainid);
+
+    args = [];
+
+    const Pair = await deploy("PairFHE", {
+        args: args,
+        from: deployer,
+        log: true,
+    });
+
+    log(`The address of the dpeloyed contract ${Pair.address}`);
+    log("------------------------------------------------------------------------");
+
+}
+
+module.exports.tags = ["all", "PairFHE"];
