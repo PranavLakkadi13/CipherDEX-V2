c1e288a7712e9ed5d76536120eb11a0135b12317
Pranav <pranavlakkadi@gmail.com>
2024-02-02 00:46:00 +0530

    update

--- test/PairFHEFixture.test.js
+++ test/PairFHEFixture.test.js
@@ -1,10 +1,11 @@
+const { ethers } = require("hardhat");
+
 async function deployPair() {
-    const accounts = await hre.ethers.getSigners();
+    const accounts = await ethers.getSigners();
     const contractOwner = accounts[0];
 
-    const contractFactory = await ethers.getContractFactory("Pair");
-    const contract = await contractFactory.connect(contractOwner).deploy(); 
-    await contract.waitForDeployment();
+    const contractFactory = await ethers.deployContract("Pair",contractOwner);
+    // const contract = await contractFactory.deploy(); 
     const address = await contract.getAddress();
 
     console.log("Contract has been deployed!!!")
